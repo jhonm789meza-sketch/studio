c8f519d89b5f60b388a810fb0206e0d9379d2f90
Jhon Meza <jhonm789.meza@gmail.com>
2025-09-11 03:28:17 +0000

    Console Error
    FirebaseError: Failed to get document because the client

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApp, FirebaseApp } from 'firebase/app';
-import { getFirestore, Firestore } from 'firebase/firestore';
+import { getFirestore, Firestore, enableIndexedDbPersistence } from 'firebase/firestore';
 
 const firebaseConfig = {
   "projectId": "rifaexpress",
@@ -12,12 +12,33 @@ const firebaseConfig = {
 };
 
 let app: FirebaseApp;
+let db: Firestore;
+
 try {
   app = getApp();
+  db = getFirestore(app);
 } catch (e) {
   app = initializeApp(firebaseConfig);
+  db = getFirestore(app);
 }
 
-const db: Firestore = getFirestore(app);
+// Enable offline persistence
+try {
+    enableIndexedDbPersistence(db)
+      .catch((err) => {
+        if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          console.warn('Firestore persistence failed: Multiple tabs open.');
+        } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+           console.warn('Firestore persistence failed: Browser does not support persistence.');
+        }
+      });
+} catch(e) {
+    console.error("Error enabling Firestore persistence", e)
+}
+
 
 export { db };
