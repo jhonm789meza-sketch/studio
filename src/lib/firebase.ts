2d89961990270345d0dc42547722da686e30f151
Jhon Meza <jhonm789.meza@gmail.com>
2025-09-14 19:46:30 +0000

    Console Error
    [2025-09-14T19:48:19.763Z]  @firebase/firestore: "Firest

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -24,18 +24,20 @@ try {
 
 // Enable offline persistence
 try {
-    enableIndexedDbPersistence(db)
-      .catch((err) => {
-        if (err.code == 'failed-precondition') {
-          // Multiple tabs open, persistence can only be enabled
-          // in one tab at a a time.
-          console.warn('Firestore persistence failed: Multiple tabs open.');
-        } else if (err.code == 'unimplemented') {
-          // The current browser does not support all of the
-          // features required to enable persistence
-           console.warn('Firestore persistence failed: Browser does not support persistence.');
-        }
-      });
+    if (typeof window !== 'undefined') {
+        enableIndexedDbPersistence(db)
+          .catch((err) => {
+            if (err.code == 'failed-precondition') {
+              // Multiple tabs open, persistence can only be enabled
+              // in one tab at a a time.
+              console.warn('Firestore persistence failed: Multiple tabs open.');
+            } else if (err.code == 'unimplemented') {
+              // The current browser does not support all of the
+              // features required to enable persistence
+               console.warn('Firestore persistence failed: Browser does not support persistence.');
+            }
+          });
+    }
 } catch(e) {
     console.error("Error enabling Firestore persistence", e)
 }
